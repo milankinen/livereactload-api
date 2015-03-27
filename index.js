7eed5047bde15c98e5c7982cf96f318035231c3b
Matti Lankinen <m.lankinen@iki.fi>
2015-03-28 00:33:16 +0200

    API implementation

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 
 
-module.exports = {}
+module.exports = {
+
+  onLoad: function(callback) {
+    if (typeof callback !== 'function') {
+      throw new Error('"onLoad" must have a callback function');
+    }
+    withLiveReactload(function(lrload) {
+      var winOnload = window.onload;
+      if (!winOnload || !winOnload.__is_livereactload) {
+        window.onload = function() {
+          // initial load event
+          callback(lrload.state);
+          lrload.winloadDone = true;
+          if (typeof winOnload === 'function' && !winOnload.__is_livereactload) {
+            winOnload();
+          }
+        }
+      }
+      window.onload.__is_livereactload = true;
+      if (lrload.winloadDone) {
+        // reload event
+        callback(lrload.state)
+      }
+    })
+  },
+
+  setState: function(state) {
+    withLiveReactload(function(lrload) {
+      lrload.state = state;
+    })
+  },
+
+  expose: function(cls, id) {
+    withLiveReactload(function(lrload) {
+      if (lrload.makeHot) {
+        lrload.makeHot(cls, 'CUSTOM_' + id);
+      }
+    })
+  }
+
+}
+
+function withLiveReactload(cb) {
+  if (typeof window !== 'undefined') {
+    var lrload = window.__livereactload;
+    if (lrload) {
+      cb(lrload);
+    }
+  }
+}
+
